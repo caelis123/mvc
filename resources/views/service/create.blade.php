6662184c430b42982396c351f9e871c61abaea5e
caelis123 <frtgaming10@gmail.com>
2026-04-14 15:45:42 +0800

    daftar service

--- resources/views/service/create.blade.php
+++ resources/views/service/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Service - KKK Service</title>
+    <title>Daftar Service - TRK Service</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
     <style>
@@ -29,7 +29,7 @@
 
                 <div class="text-center w-1/3">
                     <a href="/" class="inline-block">
-                        <h1 class="text-xl font-bold tracking-[0.2em] uppercase text-slate-800">KKK Service</h1>
+                        <h1 class="text-xl font-bold tracking-[0.2em] uppercase text-slate-800">TRK Service</h1>
                     </a>
                 </div>
 
